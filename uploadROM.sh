f701ecdeaacd8da753515c870f1fe0aa758e1a8a
JuanTamadski <76487328+JuanTamadski@users.noreply.github.com>
2026-06-06 00:33:21 +0800

    Refactor uploadROM.sh for Fastboot and SourceForge
    Updated upload process to handle Fastboot ROMs and added SourceForge upload functionality.

--- uploadROM.sh
+++ uploadROM.sh
@@ -10,8 +10,6 @@ BRAND=$(cat $work_dir/bin/ddevice/brand.txt)
 RCLONE_CONFIG_1DRIVE="$work_dir/rclone.conf"
 ONEDRIVE_REMOTE="starxONEDRIVE"
 
-
-
 if [ "$1" == "setup" ]; then
   if [ -z "$2" ] || [ -z "$3" ] || [ -z "$4" ]; then
     echo "[ERROR] - Please provide rclone token and remote name"
@@ -24,13 +22,11 @@ if [ "$1" == "setup" ]; then
   exit 0
 elif [ "$1" == "dummy" ]; then
   rclone -v --config="$RCLONE_CONFIG_1DRIVE" copy "$work_dir/dummy.txt" "$ONEDRIVE_REMOTE:NTBuild/${uploaddir}/${VERSION}/${DEVICE_MODEL}/" || {
-    echo "[ONEDRIVE] - Error uploading file to OneDrive: $FILENAME"
+    echo "[ONEDRIVE] - Error uploading file to OneDrive: dummy.txt"
     exit 1
   }
   exit 0
 fi
-
-
 
 if [[ $(git branch --show-current) == "beta" ]]; then
     VERSION="$(cat $work_dir/Version)"
@@ -51,20 +47,45 @@ elif [[ $BRAND == "RealmeUI" ]]; then
   uploaddir="RealmeUI"
 fi
 
-hash=$(md5sum out/${NTBUILD}_${DEVICE_MODEL}_${ANDROID_VER}_OS${BASE_BUILD_ID}.zip |head -c 5)
-mv out/${NTBUILD}_${DEVICE_MODEL}_${ANDROID_VER}_OS${BASE_BUILD_ID}.zip out/${NTBUILD}_${VERSION}_${DEVICE_MODEL}_OS${BASE_BUILD_ID}_${hash}_${status}.zip
+# 1. Look for the file in $work_dir instead of out/, and include the _Fastboot suffix
+SOURCE_ZIP="$work_dir/${NTBUILD}_${DEVICE_MODEL}_${ANDROID_VER}_OS${BASE_BUILD_ID}_Fastboot.zip"
+
+if [ ! -f "$SOURCE_ZIP" ]; then
+    echo "[ERROR] - Packaged ROM not found at: $SOURCE_ZIP"
+    exit 1
+fi
+
+# 2. Safely grab the MD5 hash
+hash=$(md5sum "$SOURCE_ZIP" | awk '{print $1}' | head -c 5)
+
+# 3. Rename the file in the working directory
+output_file="$work_dir/${NTBUILD}_${VERSION}_${DEVICE_MODEL}_OS${BASE_BUILD_ID}_${hash}_${status}_Fastboot.zip"
+mv "$SOURCE_ZIP" "$output_file"
+
 echo "[SCRIPT] - Output: "
-output_file="out/${NTBUILD}_${VERSION}_${DEVICE_MODEL}_OS${BASE_BUILD_ID}_${hash}_${status}.zip"
 echo "$output_file"
 echo "[ONEDRIVE] - Uploading"
-# 1drive
-rclone -v --config="$RCLONE_CONFIG_1DRIVE" copy "$output_file" "$ONEDRIVE_REMOTE:NTBuild/${uploaddir}/${VERSION}/${DEVICE_MODEL}/" || {
-echo "[ONEDRIVE] - Error uploading file to OneDrive: $output_file"
-exit 1
+
+#echo "[SCRIPT] - Output: "
+output_file="$work_dir/${NTBUILD}_${VERSION}_${DEVICE_MODEL}_OS${BASE_BUILD_ID}_${hash}_${status}_Fastboot.zip"
+echo "$output_file"
+echo "[SOURCEFORGE] - Uploading via rsync..."
+
+# Define your SourceForge details here
+SF_USER="juanski"
+SF_PROJECT="oplus-toolbuild"
+
+# Construct the target directory path automatically using the script's variables
+TARGET_DIR="/home/frs/project/${SF_PROJECT}/${uploaddir}/${VERSION}/${DEVICE_MODEL}/"
+
+# rsync command (-a for archive mode, -v for verbose, -P for progress)
+rsync -avP -e ssh "$output_file" "${SF_USER}@frs.sourceforge.net:${TARGET_DIR}" || {
+    echo "[SOURCEFORGE] - Error uploading file to SourceForge: $output_file"
+    exit 1
 }
 
 echo "[SYSTEM] - Clean Workflow.."
-rm -rf $work_dir/out
-rm -rf $work_dir/build
+rm -rf "$work_dir/out"
+rm -rf "$work_dir/build"
 
-echo "[INFO] - Build ${NTBUILD}_${VERSION} for ${DEVICE_MODEL} successfull !"
+echo "[INFO] - Build ${NTBUILD}_${VERSION} for ${DEVICE_MODEL} successful !"
